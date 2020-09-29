676ca9d1a1d27b4caaef61a839de418e75c03122
waletayo <tayot51@gmail.com>
2020-09-29 17:42:26 +0100

    chenged build

--- test/sample.test.js
+++ test/sample.test.js
@@ -2,4 +2,8 @@ describe('it should return true', () => {
     it('should return true', function () {
         expect(true).toBe(true)
     });
+    afterAll(done => {
+        done();
+    });
+
 });
